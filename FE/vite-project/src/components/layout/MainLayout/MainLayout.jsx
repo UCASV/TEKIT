1b25de3e0d27ea21c7939dd90a299bf716200e33
JulioFloHub06 <00018824@uca.edu.sv>
2025-10-28 02:00:13 -0600

    STYLE: Corregir detaller de overflow en los header y footer

--- FE/vite-project/src/components/layout/MainLayout/MainLayout.jsx
+++ FE/vite-project/src/components/layout/MainLayout/MainLayout.jsx
@@ -4,9 +4,9 @@ import Footer from '../../common/Footer/Footer'
 
 function MainLayout() {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className="d-flex flex-column min-vh-100" style={{ overflow: 'hidden' }}>
       <Navbar />
-      <main className="flex-grow-1">
+      <main className="flex-grow-1" style={{ width: '100%', overflow: 'hidden' }}>
         <Outlet />
       </main>
       <Footer />
